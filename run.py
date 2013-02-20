d35604c567449954fdd2de947bd4823252bf94b8
fab <fdev31@gmail.com>
2013-02-20 23:29:50 +0100

    Indentation fix

--- run.py
+++ run.py
@@ -14,28 +14,31 @@ w = taskw.TaskWarrior()
 
 warriors = {}
 
+
 def getmakeroot(tid):
     if tid not in warriors:
         db = conf['databases']
-        rcfile = db+tid+'.rc'
+        rcfile = db + tid + '.rc'
         if not os.path.exists(rcfile):
             try:
-                os.mkdir(db+tid)
+                os.mkdir(db + tid)
             except OSError:
                 pass
-            open(rcfile, 'w').write('data.location='+db+tid+'\n')
-            open('%s/%s/completed.data'%(db,tid), 'w').write('')
-            open('%s/%s/pending.data'%(db,tid), 'w').write('')
-        warriors[tid] = taskw.TaskWarrior(db+tid+'.rc')
+            open(rcfile, 'w').write('data.location=' + db + tid + '\n')
+            open('%s/%s/completed.data' % (db, tid), 'w').write('')
+            open('%s/%s/pending.data' % (db, tid), 'w').write('')
+        warriors[tid] = taskw.TaskWarrior(db + tid + '.rc')
     return warriors[tid]
 
 
 def decode(txt):
     return txt.encode('latin1').decode('utf-8')
 
+
 @bottle.route('/<tid>/')
 def cb(tid):
     return bottle.static_file('index.html', root=conf['root'])
+
 
 @bottle.route('/<tid>/tasks', method=['GET', 'POST'])
 def cb(tid):
@@ -49,27 +52,28 @@ def cb(tid):
             return w.task_done(uuid=bottle.request.params['uuid'])
         else:
             # new (shouldn't it be valid utf-8 already ??)
-            d = {k: decode(v) for k,v in bottle.request.POST.items()}
+            d = {k: decode(v) for k, v in bottle.request.POST.items()}
             return w.task_add(**d)
+
 
 @bottle.route('/<tid>/edit', method='POST')
 def cb(tid):
     w = getmakeroot(tid)
     params = bottle.request.POST
-    task = {
-            'uuid': params.pk,
+    task = {'uuid': params.pk,
             params.name: params.value.replace('\n', r'[\n]') if params.name == 'description' else params.value
             }
     i, t = w.task_update(task)
     return t
 
-# get list of task list ;)
 
+# get list of task list ;)
 @bottle.route('/tasklists')
 def cb():
-    return {n:n.title() for n in os.listdir(conf['databases'])
-        if os.path.exists( os.path.join(conf['databases'], n+'.rc'))}
-    return  {'default':'Default'}
+    tasks = {n: n.title() for n in os.listdir(conf['databases'])
+             if os.path.exists(os.path.join(conf['databases'], n + '.rc'))}
+    return tasks if tasks else {'default': 'Default'}
+
 
 # static files
 
@@ -77,23 +81,27 @@ def cb():
 def cb():
     return bottle.static_file('index.html', root=conf['root'])
 
+
 @bottle.route('<pfx:re:.*>/favicon.ico')
 def cb(pfx):
-    return bottle.static_file('favicon.ico', root=conf['root']+'/static/')
+    return bottle.static_file('favicon.ico', root=conf['root'] + '/static/')
+
 
 @bottle.route('/js/<fname:path>')
 def cb(fname):
-    return bottle.static_file(fname, root=conf['root']+'/static/js/')
+    return bottle.static_file(fname, root=conf['root'] + '/static/js/')
+
 
 @bottle.route('/img/<fname:path>')
 def cb(fname):
-    return bottle.static_file(fname, root=conf['root']+'/static/img/')
+    return bottle.static_file(fname, root=conf['root'] + '/static/img/')
+
 
 @bottle.route('/css/<fname:path>')
 def cb(fname):
-    return bottle.static_file(fname, root=conf['root']+'/static/css/')
+    return bottle.static_file(fname, root=conf['root'] + '/static/css/')
+
 
 if __name__ == "__main__":
     bottle.debug(True)
     bottle.run(host='0.0.0.0', port=8888)
-
